c24a996b062397aa0cc65c75319601053404f81a
Sunray0466 <son22092000@gmail.com>
2024-12-03 13:20:31 +0100

    Sonnys work

--- Code/manual_robot.py
+++ Code/manual_robot.py
@@ -11,14 +11,22 @@ ADDR_MX_MOVING_SPEED          = 32
 ADDR_MX_PRESENT_POSITION      = 36
 ADDR_MX_PUNCH                 = 48
 PROTOCOL_VERSION              = 1.0
-DXL_IDS                       = [1,2,3,4]
-DEVICENAME                    ='COM3'
+DXL_IDS                       = [1,152,3,4]
 BAUDRATE                      = 1_000_000
 TORQUE_ENABLE                 = 1
 TORQUE_DISABLE                = 0
-portHandler = dxl.PortHandler(DEVICENAME)
+# auto connect
+for i in range(10):
+    try:
+        portHandler = dxl.PortHandler("COM%s" % i)
+        portHandler.openPort()
+        print("Connected to [COM%s]" % i)
+        break
+    except:
+        continue
+else:
+    raise Exception("No connection!")
 packetHandler = dxl.PacketHandler(PROTOCOL_VERSION)
-portHandler.openPort()
 portHandler.setBaudRate(BAUDRATE)
 
 for DXL_ID in DXL_IDS:
@@ -80,7 +88,11 @@ while True:
     else:
         # print(k)
         print(dxl_goal_position)
+
 cv2.destroyAllWindows() # Close window
 cv2.VideoCapture(0).release() # Release video device
+for DXL_ID in DXL_IDS:
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
 
 portHandler.closePort()
+
